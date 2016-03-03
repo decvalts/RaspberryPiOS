e41e6b7a2b7306cb5b19136c73f2415a5c33357c
Declan V <declan.valters@manchester.ac.uk>
2016-03-03 00:18:27 +0000

    Update gpio.s

--- source/gpio.s
+++ source/gpio.s
@@ -1,4 +1,14 @@
-.globl GetGpioAddress
+.globl GetGpioAddress ;make this block available to all other parts of the code
 GetGpioAddress:
-ldr r0,=0x20200000
-mov pc,lr
+ldr r0,=0x20200000    ;load the GPIO address into memory
+mov pc,lr             ;copy the value in lr to pc
+
+.globl SetGpioFunction
+SetGpioFunction:
+cmp r0,#53
+cmpls r1,#7
+movhi pc,lr
+
+push {lr}
+mov r2,r0
+bl GetGpioAddress
