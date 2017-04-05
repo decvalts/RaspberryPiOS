664d74124ac94fa98634218a8cded3e10b0d18a5
decvalts <declan.valters@manchester.ac.uk>
2017-04-05 22:27:43 +0100

    Replaced some GPIO funcs in mainwq

--- source/main.s
+++ source/main.s
@@ -2,17 +2,49 @@
 .globl _start
 _start:
 
+bmain
+
+;Section used to place the init section (above)
+;to the default load address at 8000_16
+.section .text
+main:
+mov sp,#0x8000
+
 ;Prepare to turn the Pi LED on
-ldr r0,=0x20200000  ;load the hex number into the r0 register of the CPU, the address of the GPIO controller
-mov r1,#1           ;put the number 1 into the r1 register
-lsl r1,#18          ;logical shift left the value in r1 by 18 binary places (bloody hell this is complicated...)
-str r1,[r0,#4]      ;add 4 to the GPIO controller address, r0, and store the value of r1 in that location
+;ldr r0,=0x20200000  ;load the hex number into the r0 register of the CPU, the address of the GPIO controller
+;mov r1,#1           ;put the number 1 into the r1 register
+;lsl r1,#18          ;logical shift left the value in r1 by 18 binary places (bloody hell this is complicated...)
+;str r1,[r0,#4]      ;add 4 to the GPIO controller address, r0, and store the value of r1 in that location
+
+;Replace above with:
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
+;to enable output to the OK LED. This works by 
+;calling the Setgpiofunction with the pin num 16
+;and the pin function code 1.
 
 ;Turn the Pi LED on
-turnOn$:
-mov r1,#1           ;put the number 1 in the r1 register       
-lsl r1,#16          ;logical shift left the value in r1 by 16 binary places
-str r1,[r0,#40]     ;write the result of the lsl out to the GPIO address at 40, which turns the led 'on' (actually off!)
+;This can now be replaced
+;turnOn$:
+;mov r1,#1           ;put the number 1 in the r1 register       
+;lsl r1,#16          ;logical shift left the value in r1 by 16 binary places
+;str r1,[r0,#40]     ;write the result of the lsl out to the GPIO address at 40, which turns the led 'on' (actually off!)
+
+;New code to turn on OK LED
+;It will turn off GPIO pin 16, and therefore
+;turn on the LED.
+pinNum .req r0
+pinVal .req r1
+mov pinNum,#16
+mov pinVal,#0
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
 
 ;Now wait, before turning it off
 mov r2,#0x3F0000
@@ -22,7 +54,10 @@ wait1$:
   bne wait1$
 
 ;now turn the LED off
-str r1,[r0,#28]
+;Old Code:
+;str r1,[r0,#28]
+;New code uses pinval:
+mov pinVal,#1
 
 ;wait again...
 mov r2,#0x3F0000
@@ -36,4 +71,6 @@ b turnOn$
 
 ;Loop indefinitely
 loop$: 
-b loop$             ;basically causes an infinite loop here. (LED will remain on)
+b loop$            
+;basically causes an infinite loop here.
+;(LED will remain on)
